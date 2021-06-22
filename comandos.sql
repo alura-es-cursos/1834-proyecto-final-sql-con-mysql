4c83008cf01897f93c6644f9c75865aafb9beb73
Alvaro Camacho <60886111+ahcamachod@users.noreply.github.com>
2021-06-22 16:16:34 -0300

    Comandos Aula 4

--- comandos.sql
+++ comandos.sql
@@ -125,3 +125,67 @@ SELECT FLOOR((RAND() * (max-min+1))+min) INTO vresultado;
 RETURN vresultado;
 END$$
 DELIMITER ;
+
+
+--PROYECTO DEL AULA ANTERIOR 3
+
+
+SELECT COUNT(*) FROM clientes;
+
+SELECT * FROM clientes;
+
+SELECT * FROM clientes LIMIT 5;
+
+SELECT * FROM clientes LIMIT 5,1;
+
+SELECT * FROM clientes LIMIT 15,1;
+
+SELECT * FROM clientes LIMIT 16,1;
+
+SELECT * FROM clientes LIMIT 0,1;
+
+SELECT * FROM clientes LIMIT 1,1;
+
+SELECT f_cliente_aleatorio() AS CLIENTE;
+
+DELIMITER $$
+CREATE FUNCTION `f_cliente_aleatorio`() RETURNS varchar(11) CHARSET utf8mb4
+BEGIN
+DECLARE vresultado VARCHAR(11);
+DECLARE vmax INT;
+DECLARE valeatorio INT;
+SELECT COUNT(*) INTO vmax FROM clientes;
+SET valeatorio = f_aleatorio(1,vmax);
+SET valeatorio = valeatorio-1;
+SELECT DNI INTO vresultado FROM clientes LIMIT valeatorio,1;
+RETURN vresultado;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE FUNCTION `f_producto_aleatorio`() RETURNS varchar(10) CHARSET utf8mb4
+BEGIN
+DECLARE vresultado VARCHAR(10);
+DECLARE vmax INT;
+DECLARE valeatorio INT;
+SELECT COUNT(*) INTO vmax FROM productos;
+SET valeatorio = f_aleatorio(1,vmax);
+SET valeatorio = valeatorio-1;
+SELECT CODIGO INTO vresultado FROM productos LIMIT valeatorio,1;
+RETURN vresultado;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE FUNCTION `f_vendedor_aleatorio`() RETURNS varchar(5) CHARSET utf8mb4
+BEGIN
+DECLARE vresultado VARCHAR(5);
+DECLARE vmax INT;
+DECLARE valeatorio INT;
+SELECT COUNT(*) INTO vmax FROM vendedores;
+SET valeatorio = f_aleatorio(1,vmax);
+SET valeatorio = valeatorio-1;
+SELECT MATRICULA INTO vresultado FROM vendedores LIMIT valeatorio,1;
+RETURN vresultado;
+END$$
+DELIMITER ;
